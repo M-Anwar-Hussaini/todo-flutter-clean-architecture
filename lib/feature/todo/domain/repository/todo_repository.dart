21feb9c653de7c716198eff13d6fb6f5a6bdbe7f
Anwar Hussaini <anwarhussaini160@gmail.com>
2025-11-24 23:25:09 +0430

    feat(todo): add TodoRepository interface to domain layer
    Create the `TodoRepository` abstract class within the domain layer. This
    new file defines the contract for basic CRUD operations (get, add, update,
    delete) on `TodoEntity` objects, serving as an abstraction to decouple
    business logic from specific data implementation details.

---
+++ lib/feature/todo/domain/repository/todo_repository.dart
@@ -0,0 +1,8 @@
+import '../entities/todo_entities.dart';
+
+abstract class TodoRepository {
+  Future<List<TodoEntity>> getTodos();
+  Future<void> addTodo(TodoEntity todo);
+  Future<void> updateTodo(TodoEntity todo);
+  Future<void> deleteTodo(String id);
+}
